9a5841888ea8ec9ffbb32982a48d857b25c8f120
falgunscorpion <166277998+falgunscorpion@users.noreply.github.com>
2026-03-29 16:01:01 +0530

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -1,1 +1,8 @@
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('int:course_id/submit/', views.submit, name='submit'),
+    path('result/int:course_id/', views.show_exam_result, name='show_exam_result'),
+]
